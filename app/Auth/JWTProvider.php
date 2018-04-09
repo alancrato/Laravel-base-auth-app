4b03897127dfb329983a7876d65bc3379b93e7ed
Alan Rodrigues <alancrato@hotmail.com>
2018-04-09 15:37:28 -0300

    Criando auto refresh token

--- app/Auth/JWTProvider.php
+++ app/Auth/JWTProvider.php
@@ -4,10 +4,25 @@ namespace App\Auth;
 
 use Dingo\Api\Auth\Provider\Authorization;
 use Dingo\Api\Routing\Route;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWT;
 
 class JWTProvider extends Authorization
 {
+    /**
+     * @var JWT
+     */
+    private $jwt;
+
+    /**
+     * JWTProvider constructor.
+     * @param JWT $jwt
+     */
+    public function __construct(JWT $jwt)
+    {
+        $this->jwt = $jwt;
+    }
 
     /**
      * Get the providers authorization method.
@@ -29,6 +44,17 @@ class JWTProvider extends Authorization
      */
     public function authenticate(Request $request, Route $route)
     {
-        return \Auth::guard('api')->authenticate();
+        try {
+            return \Auth::guard('api')->authenticate();
+        }catch (AuthenticationException $exception){
+           $this->refreshToken();
+           return \Auth::guard('api')->user();
+        }
+    }
+
+    protected function refreshToken()
+    {
+        $token = $this->jwt->parseToken()->refresh();
+        $this->jwt->setToken($token);
     }
 }
