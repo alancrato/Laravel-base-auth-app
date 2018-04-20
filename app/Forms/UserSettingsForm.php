1002ad5f17c31ed24505d9be96f4f30a76ae0759
Alan Rodrigues <alancrato@hotmail.com>
2018-04-20 11:19:31 -0300

    user settings api

--- app/Forms/UserSettingsForm.php
+++ app/Forms/UserSettingsForm.php
@@ -9,9 +9,9 @@ class UserSettingsForm extends Form
     public function buildForm()
     {
         $this
-            ->add('password', 'password',[
-                'rules' => 'required|min:6|max:16|confirmed'
+            ->add('password', 'password', [
+               'rules' => 'required|min:6|max:16|confirmed'
             ])
-            ->add('password_confirmation', 'password');
+            ->add('password_confirmation','password');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
            ApiRoute::get('/user', function (Request $request){
               return $request->user('api');
            });
+           ApiRoute::patch('/user/settings', 'UsersController@updateSettings');
        });
    });
 });
